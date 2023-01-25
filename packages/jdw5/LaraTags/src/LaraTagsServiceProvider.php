0c166d79dc735c092553b85aa801d81eac301c75
jdw5 <joshuadwallace1@gmail.com>
2023-01-26 00:23:35 +1000

    Package service provider

---
+++ packages/jdw5/LaraTags/src/LaraTagsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace jdw5\LaraTags;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraTagsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
